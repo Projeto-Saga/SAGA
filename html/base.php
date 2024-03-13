c978cff6bd9a73f3923a1f61e3d7da54bfe8fd1e
Arthur <arthurgmarques@gmail.com>
2024-03-13 17:38:04 -0300

    Alterações de Estilo - 13/03/24 17:38

--- html/base.php
+++ html/base.php
@@ -3,7 +3,7 @@
         <img src="img/logo-n-forwhite.png">
     </a>
 
-    <div id="top_bar" class="clmalign" style="margin-right:101px;">
+    <div id="top_bar" class="clmalign" style="margin-right:101px">
         <div class="rowalign">
             <a class="topbar-link" href="mybook">NOTAS E FALTAS</a>
             <a class="topbar-link" href="calendar">CALENDÁRIO</a>
@@ -22,30 +22,28 @@
                 <br>
 
                 <a href="profile" class="stud-link">Meu Perfil</a>
-                <hr style="width:calc(100% - 2px);">
+                <hr style="width:calc(100% - 2px)">
                 <a href="php/logout" class="stud-link">Desconectar</a>
             </div>
         </div>
-        <div class="stud-ball" style="background-image:url(img/fotos/<?php echo $imge; ?>);"></div>
+        <div class="stud-ball" style="background-image:url(img/fotos/<?php echo $imge; ?>)"></div>
     </div>
 </div>
 
 <div id="sidebar" class="sidebar clmalign collapse">
-    <img class="sidebar-menu" src="img/hbm-menuicon.png" onclick="show();">
+    <img class="sidebar-menu" src="img/hbm-menuicon.png" onclick="show()">
 
-    <div class="clmalign">
+    <div class="clmalign" style="gap:15px">
         <a class="sidebar-link" href="mybook">
-            <img class="sidebar-icon" src="img/notas-icon.png">
+            <div class="sidebar-icon"><img src="img/notas-icon.png"></div>
             <div class="sidebar-label invobjct">Notas e Faltas</div>
         </a>
-        <hr class="sidebar-hr">
         <a class="sidebar-link" href="calendar">
-            <img class="sidebar-icon" src="img/calendario-icon.png">
+            <div class="sidebar-icon"><img src="img/calendario-icon.png"></div>
             <div class="sidebar-label invobjct">Calendário</div>
         </a>
-        <hr class="sidebar-hr">
         <a class="sidebar-link" href="requests">
-            <img class="sidebar-icon" src="img/secretaria-icon.png">
+            <div class="sidebar-icon"><img src="img/secretaria-icon.png"></div>
             <div class="sidebar-label invobjct">Secretaria</div>
         </a>
     </div>
--- js/homepage.js
+++ js/homepage.js
@@ -4,7 +4,7 @@ document.getElementById("radio1").checked = true;
 setInterval( function()
 {
     nextImage();
-}, 5000)
+}, 5000);
 
 function nextImage()
 {
