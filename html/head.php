855d9d505c4fd828df073a41c0cd02c683dfc8fb
GuiOld <guilhermehero20@gmail.com>
2025-09-27 16:46:48 -0300

    [TEST] - PWA WebView

--- html/head.php
+++ html/head.php
@@ -13,6 +13,9 @@
 <link rel="stylesheet" href="css/rspnsv.css">
 <link rel="stylesheet" href="css/mtrial.css">
 
+<link rel="manifest" href="/SAGA/manifest.json">
+<meta name="theme-color" content="#007bff">
+
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
 
 <script type="text/javascript" src="js/dataformat.js"></script>
@@ -84,4 +87,13 @@ if (isset($_SESSION['ativ']))
 <script src="https://vlibras.gov.br/app/vlibras-plugin.js"></script>
 <script>
   new window.VLibras.Widget('https://vlibras.gov.br/app');
+</script>
+<script>
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/SAGA/service-worker.js')
+      .then(reg => console.log('Service Worker registrado:', reg))
+      .catch(err => console.error('Erro ao registrar SW:', err));
+  });
+}
 </script>
--- php/connect.php
+++ php/connect.php
@@ -1,4 +1,4 @@
-<?php  return mysqli_connect('sql209.infinityfree.com', 'if0_40039126', 'projetosaga123', 'if0_40039126_saga_db'); 
+<?php return mysqli_connect('sql209.infinityfree.com', 'if0_40039126', 'projetosaga123', 'if0_40039126_saga_db'); 
 /*
 return mysqli_connect('localhost', 'root', '', 'saga_db'); ?>
 */
