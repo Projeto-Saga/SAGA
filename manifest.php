c371c3aea5d2813a44a11e8312d1a03a560e52c0
GuiOld <guilhermehero20@gmail.com>
2025-09-27 17:57:29 -0300

    Revert "Update manifest.php"
    This reverts commit 67071137fe228df9f742a4c463cc232d1bb35bdb.

--- manifest.php
+++ manifest.php
@@ -1,22 +1,3 @@
 <?php
 header('Content-Type: application/json');
-echo json_encode([
-    "name" => "Saga",
-    "short_name" => "Saga",
-    "start_url" => "/index.php",
-    "display" => "standalone",
-    "background_color" => "#ffffff",
-    "theme_color" => "#007bff",
-    "icons" => [
-        [
-            "src" => "/img/logos/logo-n-colors-192.png",
-            "sizes" => "192x192",
-            "type" => "image/png"
-        ],
-        [
-            "src" => "/img/logos/logo-n-colors-512.png",
-            "sizes" => "512x512",
-            "type" => "image/png"
-        ]
-    ]
-]);
+readfile(__DIR__. '/manifest.json');
