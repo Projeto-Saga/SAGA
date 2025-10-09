3a03c4fd00f07e955c2110736ae3c1ee633f5433
GuiOld <guilhermehero20@gmail.com>
2025-10-08 22:24:52 -0300

    Update connect.php

--- php/connect.php
+++ php/connect.php
@@ -1,5 +1,5 @@
-<?php /*return mysqli_connect('sql209.infinityfree.com', 'if0_40039126', 'projetosaga123', 'if0_40039126_saga_db'); 
-*/
+<?php return mysqli_connect('sql209.infinityfree.com', 'if0_40039126', 'projetosaga123', 'if0_40039126_saga_db'); 
+/*
 return mysqli_connect('localhost', 'root', '', 'saga_db'); ?>
-
+*/
 ?>
